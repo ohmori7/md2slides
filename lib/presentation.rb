47a4d777223c8ac74529b3a0ca294d50619490bc
Motoyuki OHMORI <ohmori@tottori-u.ac.jp>
2025-04-29 12:58:50 +0900

    rename.

--- lib/presentation.rb
+++
@@ -1,6 +0,0 @@
-$:.unshift File.dirname(File.dirname(File.realpath(__FILE__)))
-require 'config/config'
-require 'presentation/presentation'
-require 'presentation/text_to_speech'
-require 'presentation/audio'
-require 'presentation/video'
