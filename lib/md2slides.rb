7215ee8111e43f6d52783a34f6eb8152ca204723
Motoyuki OHMORI <ohmori@tottori-u.ac.jp>
2025-04-29 13:01:43 +0900

    include missing library file.

--- lib/md2slides.rb
+++ lib/md2slides.rb
@@ -1,5 +1,6 @@
 $:.unshift File.dirname(File.dirname(File.realpath(__FILE__)))
 require 'config/config'
+require 'md2slides/md'
 require 'md2slides/md2slides'
 require 'md2slides/text_to_speech'
 require 'md2slides/audio'
