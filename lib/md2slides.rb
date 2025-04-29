f6389661f736e7d88c9f23f939f13da1aeac568e
Motoyuki OHMORI <ohmori@tottori-u.ac.jp>
2025-04-29 13:06:24 +0900

    rename.

--- lib/md2slides.rb
+++ lib/md2slides.rb
@@ -1,7 +1,7 @@
 $:.unshift File.dirname(File.dirname(File.realpath(__FILE__)))
 require 'config/config'
 require 'md2slides/md'
-require 'md2slides/md2slides'
+require 'md2slides/presentation'
 require 'md2slides/text_to_speech'
 require 'md2slides/audio'
 require 'md2slides/video'
